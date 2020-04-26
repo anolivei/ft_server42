10c4e6ab8927f480a6e5e63862aef62df79263eb
Angelica Miranda <am@MacBook-Air-de-Angelica.local>
2020-04-26 04:27:26 -0300

    ft_server

--- srcs/start.sh
+++ srcs/start.sh
@@ -6,7 +6,7 @@
 #    By: anolivei <anolivei@student.42sp.org>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/04/25 14:54:55 by anolivei          #+#    #+#              #
-#    Updated: 2020/04/25 17:25:24 by anolivei         ###   ########.fr        #
+#    Updated: 2020/04/26 04:19:46 by anolivei         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,5 +14,21 @@
 /etc/init.d/nginx start
 /etc/init.d/mysql start
 /etc/init.d/php7.3-fpm start
+
+echo "
+                    ##        .            
+              ## ## ##       ==            
+           ## ## ## ##      ===            
+       /""""""""""""""""\___/ ===        
+  ~~~ {~~ ~~~~ ~~~ ~~~~ ~~ ~ /  ===- ~~~   
+       \______ o          __/            
+         \    \        __/             
+          \____\______/                
+ 
+          |          |
+       __ |  __   __ | _  __   _
+      /  \| /  \ /   |/  / _\ | 
+      \__/| \__/ \__ |\_ \__  |
+"
 # Linha para monitoramento de logs, matem o servidor ligado
 tail -f /var/log/nginx/access.log /var/log/nginx/error.log
